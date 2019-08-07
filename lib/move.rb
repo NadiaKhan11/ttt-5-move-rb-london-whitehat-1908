8c8f7861dc77856fa32c990d76dc8b84c7a71609
Nadia Khan <nadia.khan@sky.uk>
2019-08-07 14:14:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,8 @@ def input_to_index(input)
   input = .to_i -1
 end  
   
-def move(board, index, user_marker = "X")
-  board[index.to_i] = user_marker
+
+board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
 end
